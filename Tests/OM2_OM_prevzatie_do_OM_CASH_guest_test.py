492be2736753149b0d3eccb1aad73ddc1a73e167
Erik Valigurský <erik.vali@icloud.com>
2025-05-19 13:33:41 +0200

    Sprehlasnenie testu a pridanie popisov, pre lepsiu orientaciu

--- Tests/OM2_OM_prevzatie_do_OM_CASH_guest_test.py
+++ Tests/OM2_OM_prevzatie_do_OM_CASH_guest_test.py
@@ -4,13 +4,17 @@ from playwright.sync_api import Page, expect
 
 
 
-# Tento test odosle objednavku z OM na OM - OM2_OM_prevzatie_do_OM_CASH_guest_test.py (Plata pri prevzati)
-# Este bude treba osetrit spomalenost stranky a problemy s tym spojene
+# Tento test odosle objednavku z OM na OM - OM2_OM_prevzatie_do_OM_CASH_guest_test.py (Platba pri prevzati)
+
 def test_om2om_(page: Page) -> None:
 
-# Open page DPDmojkurier.sk
+    # Open page DPDmojkurier.sk
     page.goto("https://twww.dpdmojkurier.sk/")
+
+    # Prijatie cookies
     page.get_by_role("button", name="Prijať všetko").click()
+
+    # Vytvorenie bjednavky na OM DPD Bratislava (Odosielatel)
     page.get_by_role("link", name="Poslať zásielku").first.click()
     page.get_by_role("button", name="Pokračovať").first.click()
     page.get_by_role("textbox", name="Meno").fill("Erik")
@@ -26,6 +30,8 @@ def test_om2om_(page: Page) -> None:
     page.get_by_text("Pobočka DPD BRATISLAVA").click()
     page.get_by_role("checkbox", name="Prosím, pre ďalší krok a").check()
     page.get_by_role("button", name="Pokračovať").click()
+
+    # Vytvorenie bjednavky na OM DPD Bratislava (Prijimatel)
     page.get_by_role("textbox", name="Meno").click()
     page.get_by_role("textbox", name="Meno").fill("Test")
     page.get_by_role("textbox", name="Priezvisko").fill("Test")
@@ -39,6 +45,8 @@ def test_om2om_(page: Page) -> None:
     page.get_by_role("textbox", name="Ulica").fill("Ulica")
     page.get_by_role("textbox", name="Popisné číslo").fill("1")
     page.get_by_role("button", name="Pokračovať").click()
+
+    # Pridanie balika
     page.get_by_role("img", name="add-parcel-icon").click()
     page.locator("div").filter(has_text=re.compile(r"^váha:do 5 kgdĺžka:55 cmšírka:45 cmvýška:20 cmPridať$")).get_by_role("button").click()
     page.get_by_role("button", name="Pokračovať").click()
@@ -48,12 +56,12 @@ def test_om2om_(page: Page) -> None:
     page.get_by_role("radio", name="Platba na odbernom mieste DPD").check()
     page.get_by_role("button", name="Potvrdiť objednávku").click()
 
-# Expect a title "to contain" a substring.
+    # Ocakava nadpis "Hotovo"
     expect(page.get_by_role("heading", name="Hotovo")).to_be_visible()
 
 
 
-
+# Prijatie balika cez OM v CashApp
 def test_login_prijatie_balika(page: Page) -> None:
     page.goto("https://twww.dpdmojkurier.sk")  # Prejdite na URL
     # Prijat cookies
@@ -105,6 +113,6 @@ def test_login_prijatie_balika(page: Page) -> None:
     page.get_by_role("radio", name="Hotovosť").check()
     page.get_by_role("button", name="Zásielka uhradená").click()
 
-    # Expect a title "to contain" a substring.
+    # Očakáva nadpis "Úspešne odoslané"
     expect(page.get_by_role("heading", name="Úspešne odoslané")).to_be_visible()
 
